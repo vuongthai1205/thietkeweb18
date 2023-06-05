232f0ff96717bbc6562efda8621f5821b379a40a
vuongthai1205 <2051010367vuong@ou.edu.vn>
2023-06-05 22:48:47 +0700

    first commit2223333

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-    BrowserRouter
-} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 window.scrollTo(0, 0);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-<React.StrictMode >
-    <BrowserRouter basename = "/" > { /*<BrowserRouter>*/ } <App / >
-    </BrowserRouter> 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter basename="/">
+            {" "}
+            {/*<BrowserRouter>*/} <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
 
